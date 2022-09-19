80ad35c539f457656f2689d10725a035e769813e
Wolfgang <wolfgangdslaurent@gmail.com>
2022-09-19 20:26:13 +0100

    Updates

---
+++ precache-manifest.4ca55743d041c0b041fbd1624f9b91f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1de8d2d42c35c37a3dbe",
+    "url": "/thecar/static/css/main.79fe0f84.chunk.css"
+  },
+  {
+    "revision": "1de8d2d42c35c37a3dbe",
+    "url": "/thecar/static/js/main.1de8d2d4.chunk.js"
+  },
+  {
+    "revision": "5d53b219d3e4842bf86d",
+    "url": "/thecar/static/js/1.5d53b219.chunk.js"
+  },
+  {
+    "revision": "a331f22eece4ceade70b",
+    "url": "/thecar/static/js/runtime~main.a331f22e.js"
+  },
+  {
+    "revision": "537e44661dccb6274a6635bd8d15e748",
+    "url": "/thecar/static/media/background.537e4466.mp4"
+  },
+  {
+    "revision": "5a888265fa336ab11e89a5d0b242cbb8",
+    "url": "/thecar/static/media/placeholder.5a888265.jpg"
+  },
+  {
+    "revision": "5ea2cee430031143be2f295e8e201c35",
+    "url": "/thecar/static/media/suffolkpunch.5ea2cee4.ttf"
+  },
+  {
+    "revision": "fb2465f41520f6edd5f4b9a1d96825a6",
+    "url": "/thecar/index.html"
+  }
+];
