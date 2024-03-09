08dbce96c252e813ad10de37f655f98de4cd13df
Wolfgang <wolfgangdslaurent@gmail.com>
2024-03-09 18:57:43 +0000

    deploy

---
+++ build/precache-manifest.bb7ecede026a695db444a30b915f29a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "80b3cc917ee78830a1f4",
+    "url": "/static/css/main.e6eb4eac.chunk.css"
+  },
+  {
+    "revision": "80b3cc917ee78830a1f4",
+    "url": "/static/js/main.80b3cc91.chunk.js"
+  },
+  {
+    "revision": "5d53b219d3e4842bf86d",
+    "url": "/static/js/1.5d53b219.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "537e44661dccb6274a6635bd8d15e748",
+    "url": "/static/media/background.537e4466.mp4"
+  },
+  {
+    "revision": "5a888265fa336ab11e89a5d0b242cbb8",
+    "url": "/static/media/placeholder.5a888265.jpg"
+  },
+  {
+    "revision": "5ea2cee430031143be2f295e8e201c35",
+    "url": "/static/media/suffolkpunch.5ea2cee4.ttf"
+  },
+  {
+    "revision": "7dad781efec1bcad08a95996e580b21c",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/thecar/precache-manifest.5b279796f9b9065eee667fe6476a5895.js"
+  "/precache-manifest.bb7ecede026a695db444a30b915f29a2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/thecar/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
